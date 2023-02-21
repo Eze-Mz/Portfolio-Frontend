7a87438254c199565fe139cfecbf7e3b74f8bbb1
Eze Muñoz <eze.munoz@mi.unc.edu.ar>
2023-02-21 16:51:21 -0300

    apiUrl changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://warm-gorge-04744.herokuapp.com',
+  apiUrl: 'https://app-cf323fa7-aaa1-4a4a-8731-9683da2b32e3.cleverapps.io/',
 };
